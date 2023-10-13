397b93a04b3374844298ca440f9bf4c7c170da78
Keshav Agrawal <Keshavagrawal9471408042@gmail.com>
2023-10-13 21:10:27 +0530

    Login and register html pages created and css added

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/home/index.html");
+});
 
-app.get("/", (req, res) =>{
-    // res.send("<h1>Hello World</h1>");
-    res.sendFile(__dirname + "/public/index.html")
-})
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/accounts/register.html");
+});
 
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/accounts/login.html");
+});
 
-app.listen(PORT, ()=>{
- console.log("App is Listening on port " + PORT)
- console.log("http://localhost:" + PORT);
-})
+app.listen(PORT, () => {
+  console.log("App is Listening on port " + PORT);
+  console.log("http://localhost:" + PORT);
+});
