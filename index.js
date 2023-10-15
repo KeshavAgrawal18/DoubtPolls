1b6ce9cc564025ba2e5e189222b683cc132bc2e0
Keshav Agrawal <Keshavagrawal9471408042@gmail.com>
2023-10-15 06:22:11 +0530

    Add environment file and vars

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/home/index.html");
-});
 
-app.get("/register", (req, res) => {
-  res.sendFile(__dirname + "/public/accounts/register.html");
-});
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/public/home/index.html")
+})
 
-app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/accounts/login.html");
-});
+app.get("/register", (req, res) =>{
+    res.sendFile(__dirname + "/public/accounts/register.html")
+})
 
-app.listen(PORT, () => {
-  console.log("App is Listening on port " + PORT);
-  console.log("http://localhost:" + PORT);
-});
+app.get("/login", (req, res) =>{
+    res.sendFile(__dirname + "/public/accounts/login.html")
+})
+
+
+app.listen(PORT, ()=>{
+ console.log("App is Listening on port " + PORT)
+ console.log("http://localhost:" + PORT);
+})
